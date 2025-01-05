91eebc07b9afa50325a8f796187d5042968350b4
Koki Iwai <koki4302000@gmail.com>
2025-01-06 02:18:33 +0900

    add README on the way

--- test/test.bash
+++ test/test.bash
@@ -7,11 +7,11 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-ros2 run mypkg ipaddress & pub=i$!
+ros2 run mypkg ipaddress & pub=$!
 
 timeout 10 ros2 topic echo /ip_address > /tmp/mypkg.log
 
 kill $pub
 
 cat /tmp/mypkg.log | 
-grep 'hostname' 
+grep -e 'hostname' -e 'address'
