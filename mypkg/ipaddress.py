1b68a106cea07ff5454185952868e38ce50874b0
Koki Iwai <koki4302000@gmail.com>
2025-01-05 16:11:53 +0900

    change to class

--- mypkg/ipaddress.py
+++ mypkg/ipaddress.py
@@ -3,29 +3,34 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import socket
 
-rclpy.init()
-node = Node("ip_address_pub")
-pub= node.create_publisher(String, "ip_address", 10)
+class Ipaddress(Node):
+    def __init__(self):
+        super().__init__("get_ipaddress_pub")
+        self.pub = self.create_publisher(String, "ip_address", 10)
+        self.create_timer(0.5, self.cb)
 
-def get_ip_address():
-    try:
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
-    except Exception as e:
-        print("Error")
-        return None
+    def cb(self):
+        check = self.get_ipaddress()
+        if check:
+            msg = String()
+            msg.data = self.ip_address
+            self.pub.publish(msg)
+            self.get_logger().info(f'{self.ip_address}')
+        else:
+            self.get_logger().warn('Failed')
 
-def cb():
-    ip_address = get_ip_address()
-    if ip_address:
-        msg = String()
-        msg.data = ip_address
-        pub.publish(msg)
-        node.get_logger().info(f'{ip_address}')
-    else:
-        node.get_logger().warn('Failed')
+
+
+    def get_ipaddress(self):
+        try:
+            self.hostname = socket.gethostname()
+            self.ip_address = socket.gethostbyname(self.hostname)
+            return self.ip_address
+        except Exception as e:
+            print("Error")
+            return None
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Ipaddress()
     rclpy.spin(node)
