579602396854d7cdb26e6bd61cd18fdbb2155eac
Koki Iwai <koki4302000@gmail.com>
2025-01-09 01:30:41 +0900

    complete test may be

--- test/test.bash
+++ test/test.bash
@@ -13,8 +13,10 @@ timeout 10 ros2 topic echo /ip_address > /tmp/mypkg.log
 
 kill -9 $pub
 
-cat /tmp/mypkg.log | 
-grep -e 'hostname' -e 'address'
+if cat /tmp/mypkg.log | grep "'" ; then
+    exit 1
+else
+    exit 0
+fi
 
-cat /tmp/mypkg.log |
-grep "\'" && exit 1
+
